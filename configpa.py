c20f654c8172697f1ac0656e759cf16901107d02
SaydsC <G00411297@gmit.ie>
2023-12-29 16:26:13 +0000

    config file

---
+++ configpa.py
@@ -0,0 +1,6 @@
+mysqldb = {
+    'host':"sadieconcannon.mysql.pythonanywhere-services.com",   
+    'user':"sadieconcannon",    
+    'password':"S@die!11!",
+    'database':"sadieconcannon$humanresourcesdb"
+}
